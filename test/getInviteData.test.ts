33f462316447b58d59e705c3d7c46e305cf27a22
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-06 15:22:08 +1200

    basic api stuff

--- test/getInviteData.test.ts
+++ test/getInviteData.test.ts
@@ -3,7 +3,7 @@ import { DiscordAPI } from '../src';
 describe('DiscordAPI/getInviteData', () => {
     it('gets guild data', async () => {
         const res = await DiscordAPI.getInviteData('dsfpFTR4gw');
-        if (!res.success) fail('Should be successful');
+        if (!res.success) throw new Error('Should be successful');
         expect(res.data.guild?.id).toEqual('815732601302155275');
         expect(res.data.inviter?.id).toEqual('240312568273436674');
         expect(res.data.approximate_member_count).toBeDefined();
@@ -13,7 +13,7 @@ describe('DiscordAPI/getInviteData', () => {
         const res = await DiscordAPI.getInviteData('someFakeInviteCode');
 
         /** Fun fact Typescript shits itself if I don't have the `=== true` here. */
-        if (res.success === true) fail('Should have failed');
+        if (res.success === true) throw new Error('Should have failed');
         expect(res.error.response?.data?.code).toBe(10006); // code for 'unknown invite'
     });
 });
