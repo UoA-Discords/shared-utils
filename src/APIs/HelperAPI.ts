eef887bd40fd98900d2e3b25391ca4d712de6cb6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-09 02:35:38 +1200

    poggy thorax

--- src/APIs/HelperAPI.ts
+++ src/APIs/HelperAPI.ts
@@ -1,3 +1,18 @@
+import DiscordAPI from './DiscordAPI';
+
 export default abstract class HelperAPI {
-    //
+    /** Invite validation middleware.
+     *
+     * @param {string} inviteUrl - Invite code (e.g. `abc123`).
+     */
+    public static async verifyInvite(inviteUrl: string): Promise<true | string> {
+        const invite = await DiscordAPI.getInviteData(inviteUrl);
+        if (!invite.success) return `noGuild`;
+
+        if (invite.data.expires_at !== null) return `expires`;
+
+        if (invite.data.approximate_member_count < 100) return `smol`;
+
+        return true;
+    }
 }
