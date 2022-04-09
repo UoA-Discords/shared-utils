00bba0496afcd8d1834c07492cef1c9b2dfd9afb
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-09 15:25:19 +1200

    overhaul guild verify helper method

--- src/APIs/HelperAPI.ts
+++ src/APIs/HelperAPI.ts
@@ -1,22 +1,15 @@
 import { Invite } from '../DiscordTypes';
-import { ApplicationResponses } from '../Types';
-import DiscordAPI from './DiscordAPI';
-
-type VerifyInviteResponse = ApplicationResponses.Expires | ApplicationResponses.TooSmall | ApplicationResponses.Invalid;
 
 export default abstract class HelperAPI {
     /** Invite validation middleware.
      *
      * @param {string} inviteUrl - Invite code (e.g. `abc123`).
      */
-    public static async verifyInvite(inviteUrl: string): Promise<Invite | VerifyInviteResponse> {
-        const invite = await DiscordAPI.getInviteData(inviteUrl);
-        if (!invite.success) return ApplicationResponses.Invalid;
+    public static verifyGuild(invite: Invite): true | `expires` | `toosmall` {
+        if (invite.expires_at !== null) return `expires`;
 
-        if (invite.data.expires_at !== null) return ApplicationResponses.Expires;
+        if (invite.approximate_member_count < 100) return `toosmall`;
 
-        if (invite.data.approximate_member_count < 100) return ApplicationResponses.TooSmall;
-
-        return invite.data;
+        return true;
     }
 }
