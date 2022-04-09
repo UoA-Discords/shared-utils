72b350c53bcbeb3778be79f14649284c1d94018d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-09 12:04:26 +1200

    typed invite response

--- src/APIs/HelperAPI.ts
+++ src/APIs/HelperAPI.ts
@@ -1,17 +1,19 @@
 import DiscordAPI from './DiscordAPI';
 
+type VerifyInviteResponse = `noGuild` | `hasExpiry` | `tooSmall`;
+
 export default abstract class HelperAPI {
     /** Invite validation middleware.
      *
      * @param {string} inviteUrl - Invite code (e.g. `abc123`).
      */
-    public static async verifyInvite(inviteUrl: string): Promise<true | string> {
+    public static async verifyInvite(inviteUrl: string): Promise<true | VerifyInviteResponse> {
         const invite = await DiscordAPI.getInviteData(inviteUrl);
         if (!invite.success) return `noGuild`;
 
-        if (invite.data.expires_at !== null) return `expires`;
+        if (invite.data.expires_at !== null) return `hasExpiry`;
 
-        if (invite.data.approximate_member_count < 100) return `smol`;
+        if (invite.data.approximate_member_count < 100) return `tooSmall`;
 
         return true;
     }
