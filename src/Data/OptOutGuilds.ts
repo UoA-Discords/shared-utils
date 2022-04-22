ef84a182a849790221c5bc26c1b14c759f8e5d6d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-22 13:24:32 +1200

    add opt out

--- src/Data/OptOutGuilds.ts
+++ src/Data/OptOutGuilds.ts
@@ -4,6 +4,8 @@
  * Guilds wanting to be in this list should contact a developer.
  *
  */
-const OptOutGuilds = new Set<string>([]);
+const OptOutGuilds = new Set<string>([
+    `947762829845467136`, // S1 CompSci 101 (2022)
+]);
 
 export default OptOutGuilds;
